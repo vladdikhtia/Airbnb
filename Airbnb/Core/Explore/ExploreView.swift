b18fe5a5ae0732c190f088e345931979e349f76d
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-12 23:19:01 +0200

    Added ExploreView with Search bar

--- Airbnb/Core/Explore/ExploreView.swift
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -11,13 +11,30 @@ struct ExploreView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
+                SearchAndFilterBar()
+                
                 LazyVStack(spacing: 32) {
                     ForEach(0..<10, id: \.self) { listing in
-                        ListingItemView()
-                            .frame(height: 400)
+                        NavigationLink(value: listing) {
+                            ListingItemView()
+                                .frame(height: 400)
+                                
+                        }
+                        
+//                        NavigationLink {
+//                            EmptyView()
+//                        } label: {
+//                            ListingItemView()
+//                        }
+//                            .frame(height: 400)
+//                            .foregroundStyle(.black)
                     }
                 }
                 .padding()
+            }
+            
+            .navigationDestination(for: Int.self) { listing in
+                Text("Listing num: \(listing)")
             }
         }
     }
