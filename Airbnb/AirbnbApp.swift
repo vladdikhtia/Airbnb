6f8f6a82511aaf62d6022a19d2c4b6dba2a95252
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-12 16:36:21 +0200

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Vladyslav Dikhtiaruk on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
