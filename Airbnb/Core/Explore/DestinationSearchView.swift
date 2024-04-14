a2f849057e58e865adc47990588524d9de1afb7c
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-14 23:20:08 +0200

    Implemented DestinationSearchView
    with animations

--- Airbnb/Core/Explore/DestinationSearchView.swift
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -7,12 +7,71 @@
 
 import SwiftUI
 
+
+enum DestinationSearchOptions {
+    case location
+    case dates
+    case guests
+}
+
 struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @State var destination = ""
+    @State var fromDate = Date()
+    @State var toDate = Date()
+    @State var numGuests = 1
+    
+    @State var selectedOption: DestinationSearchOptions = .location
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack{
+                Button{
+                    withAnimation(.snappy){
+                        show = false
+                    }
+                } label: {
+                    Image(systemName: "xmark.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
+                }
+                
+                Spacer()
+                
+                if(!destination.isEmpty){
+                    Button{
+                        destination=""
+                    } label: {
+                        Text("Clear")
+                    }
+                    .foregroundStyle(.black)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
+            }
+            .padding()
+            
+            // location selection view
+            LocationSelectionView(selOption: $selectedOption, destination: $destination)
+            
+            // date selection view
+            DateSelectionView(selOption: $selectedOption, fromDate: $fromDate, toDate: $toDate)
+            
+            // selection guests view
+            SelectionGuestsView(selOption: $selectedOption, numGuests: $numGuests)
+        }
+        
+        Spacer()
     }
 }
 
 #Preview {
-    DestinationSearchView()
+    DestinationSearchView(show: .constant(false))
 }
+
+
+
+
+
+
+
