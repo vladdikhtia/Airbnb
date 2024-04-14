83087eeccafd08f0ba96480700e594dede6ca571
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-14 23:19:19 +0200

    Implemented custom collapse modifier

--- Airbnb/Extensions/CustomCollapsedModifier.swift
+++ Airbnb/Extensions/CustomCollapsedModifier.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct CustomCollapsedModifier: ViewModifier {
+    var height: CGFloat
+    var action: () -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(height: height)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.spring){
+                    action()
+                }
+            }
+    }
+}
+extension View {
+    func customCollapsedModifier(height: CGFloat, action: @escaping () -> Void) -> some View {
+        modifier(CustomCollapsedModifier(height: height, action: action))
+    }
+}
